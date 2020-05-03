9cdf32101be476abaf0e82b81345a40c3e43e05b
RSouzaRibeiro <rafaelsouza803@gmail.com>
2020-05-03 20:25:11 -0300

    add config send grid

--- src/modules/mailer.js
+++ src/modules/mailer.js
@@ -2,28 +2,31 @@ const nodemailer = require('nodemailer');
 const hbs = require('nodemailer-express-handlebars')
 const path = require('path');
 
-const { host, port, user, pass} = require("../config/mail.json");
+const { host, port, user, pass } = require("../config/mail.json");
 
 var transport = nodemailer.createTransport({
-    host,
-    port ,
-    auth: { user, pass }
-  });
+  host: process.env.SENDGRID_API_SMTP,
+  port: process.env.SENDGRID_API_PORT,
+  auth: {
+    user: process.env.SENDGRID_API_USER,
+    pass: process.env.SENDGRID_API_PASS
+  }
+});
 
-  const handlebarOptions = {
-    viewEngine: {
-      extName: '.hbs',
-      partialsDir: path.resolve('./src/resources/mail/'),
-      layoutsDir: path.resolve('./src/resources/mail/'),
-      defaultLayout: path.resolve('./src/resources/mail/auth/forgot_password'),
-    },
-    viewPath: path.resolve('./src/resources/mail/'),
-    extName: '.handlebars',
-  };
+const handlebarOptions = {
+  viewEngine: {
+    extName: '.hbs',
+    partialsDir: path.resolve('./src/resources/mail/'),
+    layoutsDir: path.resolve('./src/resources/mail/'),
+    defaultLayout: path.resolve('./src/resources/mail/auth/forgot_password'),
+  },
+  viewPath: path.resolve('./src/resources/mail/'),
+  extName: '.handlebars',
+};
 
-  transport.use('compile',hbs(handlebarOptions));
-
-
+transport.use('compile', hbs(handlebarOptions));
 
 
-  module.exports = transport;
+
+
+module.exports = transport;
