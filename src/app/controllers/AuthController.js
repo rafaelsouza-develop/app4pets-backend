8d8e8205d2f79c2dad04fd1fd11a1d9e9d1b92be
RSouzaRibeiro <rafaelsouza803@gmail.com>
2020-06-02 20:13:02 -0300

    Small corretion flow register

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -37,8 +37,10 @@ router.post('/authenticate', async (req,res) => {
     }
         
 
-    if(!await bcrypt.compare(password, user.password))
+    if(!await bcrypt.compare(password, user.password)){
         return res.status(400).send({ error: 'invalid password'})
+    }
+        
 
         user.password = undefined
 
