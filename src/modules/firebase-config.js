973a3411428629c2a8fdf13db8154547902a6229
Rafael Souza <rafaelsouza.develop@gmail.com>
2021-01-18 20:16:43 -0300

    fix: teste

--- src/modules/firebase-config.js
+++ src/modules/firebase-config.js
@@ -2,7 +2,8 @@ const admin = require('firebase-admin')
 const serviceAccount = require('../config/app4pets-dev-firebase-adminsdk.json')
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://app4pets-dev.firebaseio.com'
   })
   
   module.exports.admin = admin
