79a8775defe57861d8627304cecb484268fd7e62
Rafael Souza <rafaelsouza.develop@gmail.com>
2021-01-18 19:55:33 -0300

    fix: teste

--- src/app/controllers/PetController.js
+++ src/app/controllers/PetController.js
@@ -79,20 +79,20 @@ router.delete('/delete/:id', async (request, response) =>{
 });
 
 router.post('/notification' , async (request, response) => {
-
+    console.log(request)
     const notification_options = {
         priority: "high",
         timeToLive: 60 * 60 * 24
       };
 
-    const  registrationToken = req.body.registrationToken
-    const message = req.body.message
+    const  registrationToken = request.body.registrationToken
+    const message = request.body.message
     const options =  notification_options
 
     admin.messaging().sendToDevice(registrationToken, message, options)
     .then( response => {
-
-     res.status(200).send("Notification sent successfully")
+console.log(this.response)
+        response.status(200).send("Notification sent successfully")
      
     })
     .catch( error => {
